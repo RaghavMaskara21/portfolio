f1bead75a121bc310ccb0b90c060355f2c4cfd3d
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-03-09 23:27:19 +0530

    created basic skeleton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Experience from './Pages/Experience/Experience';
+import Footer from './Pages/Footer/Footer';
 import Home from './Pages/Home/Home';
+import Projects from './Pages/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <About/>
+      <Experience/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
