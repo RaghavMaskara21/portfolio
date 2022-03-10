e77363db0bd18dba919271d6570095dfe7be88b5
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-03-11 01:44:36 +0530

    fixed extra space

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,19 +7,19 @@ function Home() {
         
         <div className='container textblock'>
             <div className='homeContent'>
-                <div className='text1 fs-5'>
+                <div className='text1'>
                 <p>Hi there, I'm</p>
                 </div>
                 <div className='text2'>
                 <p className='name'>Raghav Maskara</p>
                 </div>
-                <div className='text3 fs-2'>
+                <div className='text3'>
                 <p>I make awesome websites</p>
                 </div>
-                <div className='text4 fs-6'>
+                <div className='text4'>
                 <p>Full stack web developer</p>
                 </div>
-                <div className='text5 fs-6'>
+                <div className='text5'>
                 <p>Based in Surat, India.</p>
                 </div>
                 <div className='text6'>
