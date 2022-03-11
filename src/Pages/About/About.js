66217ff7c5fd776a07db6e02566f0964ce0e1b19
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-03-11 19:47:21 +0530

    added react icons

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about.css'
 import mypic from '../../images/mypic.jpg'
+import { FaAngleRight } from 'react-icons/fa';
 function About() {
   return (
     <div className='container about_section' id="About">
@@ -11,15 +12,50 @@ function About() {
                 <img src={mypic} className="mypic" alt='raghav_maskara' ></img>
             </div>
             <div className='col-lg-8'>
+                <h1>Software Developer</h1>
+                {/* <p>I am a Software Developer based in India.</p> */}
                 <p>
-                Hello there, my name is Raghav Maskara and i love doing stuff for internet espacially open-source projects. My love for the development started when i was 14: i was playing a supercell game called Hayday and i wanted to automate repetitive things so i built a bot for the game (it's not fair i know 🙁). Now, i mainly develop MERN stack apps, i'm open to new opportunities.
+                Hello there, my name is Raghav Maskara and I am currently studying Computer Science Engineering and have always had a keen interest in programming and development. Since the last two years, I've been really crazy about web or full stack development and have created various projects as well. I mainly develop MERN stack apps, i'm open to new opportunities.
                 </p>
-                <div className='abtskills'>
+                <p>
+                I can't stop learning new things; the more, the better.
+                </p>
+
+
+
+                {/* <div className='abtskills'>
                 <div className='sk1'>React JS</div>
                 <div className='sk2'>Node JS</div>
                 <div className='sk3'>Express JS</div>
                 <div className='sk4'>MongoDB</div>
                 <div className='sk5'>HTML/CSS</div>
+                </div> */}
+
+                <div className='container'>
+                <div className='row'>
+                <div className='col-6'>
+                  <div>
+                    <p> <span className='righticon'> <FaAngleRight/></span> <strong> Birthday:</strong> 24 January 2001</p>
+                  </div>
+                  <div>
+                    <p> <span className='righticon'> <FaAngleRight/></span> <strong> Website:</strong> 24 January 2001</p>
+                  </div>
+                  <div>
+                    <p> <span className='righticon'> <FaAngleRight/></span> <strong> City:</strong> 24 January 2001</p>
+                  </div>
+                </div>
+                <div className='col-6'>
+                <div>
+                    <p> <span className='righticon'> <FaAngleRight/></span> <strong> Age:</strong> 24 January 2001</p>
+                  </div>
+                  <div>
+                    <p> <span className='righticon'> <FaAngleRight/></span> <strong> Email:</strong> 24 January 2001</p>
+                  </div>
+                  <div>
+                    <p> <span className='righticon'> <FaAngleRight/></span> <strong> Degree:</strong> 24 January 2001</p>
+                  </div>  
+                </div>
+                </div>  
                 </div>
             </div>    
             </div>
